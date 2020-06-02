f1848c9944c6bd5d9d6f8c82bdee08dc279303c2
sam <samaragml2222@gmail.com>
2020-06-02 11:07:52 +0530

    Dev1 changes to Team java file

--- DemoProj1/src/com/nt/test/Team.java
+++ DemoProj1/src/com/nt/test/Team.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Team {
+	public static void main(String[] args) {
+		Utility util = new Utility();
+		System.out.println(util.getSysDate());
+	}
 
 }
